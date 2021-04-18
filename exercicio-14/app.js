e2733d71b3b7908644c167b4c98d031eafa7be61
davidtheblane <davidtheblane@gmail.com>
2021-04-18 11:57:23 -0300

    :up: Finalizado Aulas 02 Etapa 5

--- exercicio-14/app.js
+++ exercicio-14/app.js
@@ -5,7 +5,9 @@
     maiúsculas.
 */
 
+const title = document.querySelector("h1");
 
+title.innerText = title.innerText.toUpperCase();
 
 /*
   02
@@ -14,7 +16,15 @@
   - Cada número deve estar dentro de uma <li> com a classe "number".
 */
 
-const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
+const numbers = [53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55];
+
+const ul = document.querySelector(".numbers");
+
+const insertNumberIntoUl = (number) => {
+  ul.innerHTML += `<li class="number"> ${number} </li>`;
+};
+
+numbers.forEach(insertNumberIntoUl);
 
 /*
   03
@@ -24,7 +34,18 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
     - Se o número é ímpar, exiba-o na cor "pink".
 */
 
+const lis = document.querySelectorAll(".number");
 
+const changeLiColor = (li) => {
+  const isEven = Number(li.textContent) % 2 === 0;
+  if (isEven) {
+    li.style.color = "lightblue";
+    return;
+  }
+
+  li.style.color = "pink";
+};
+lis.forEach(changeLiColor);
 
 /*
   04
@@ -33,8 +54,9 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
 
   P.s: a classe "body-background" já está declarada no style.css.
 */
+const body = document.body;
 
-
+console.log(body.classList.add("body-background"));
 
 /*
   05
@@ -44,15 +66,19 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
     do link do index.html.
 */
 
+const repoLink = document.querySelector("a");
 
+repoLink.setAttribute(
+  "href",
+  "https://github.com/roger-melo-treinamentos/curso-de-js-roger-melo"
+);
 
 /*
   06
 
   - Exiba o novo valor do atributo href do link no console.
 */
-
-
+console.log(repoLink.getAttribute("href"));
 
 /*
   07
@@ -61,15 +87,14 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
     manipuladas via JS no h1.
 */
 
-
+console.log(title.style);
 
 /*
   08
 
   - Remova a classe "body-background", do elemento body.
 */
-
-
+// body.classList.remove("body-background");
 
 /*
   09
@@ -77,3 +102,4 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
   - Se o link da página possuir uma classe "link", remova-a;
   - Não utilize o método remove() para fazer isso.
 */
+repoLink.classList.toggle("link");
