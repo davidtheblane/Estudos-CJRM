b83b14670cac7dbea0fec73bd1a0f412dc17e9ca
davidtheblane <davidtheblane@gmail.com>
2021-04-20 18:20:23 -0300

    trabalhando com eventos de clique

--- exercicio-15/app3.js
+++ exercicio-15/app3.js
@@ -1,47 +1,21 @@
-// const button = document.querySelector('button')
+const botao = document.querySelector("button");
 
-// button.addEventListener('click', () => {
-//   console.log('Clicou no botão!')
-// })
+botao.addEventListener("click", () => {
+  console.log("Clicou no botao");
+});
 
-//  const lis = document.querySelectorAll('li')
+const lis = document.querySelectorAll("li");
 
-//  lis.forEach(li => {
-//    li.addEventListener('click', event => {
-//       const clickedElement = event.target
-      
-//       clickedElement.remove()
-//    }) 
-//  })
+// lis.forEach((li) => {
+//   li.addEventListener("click", () => {
+//     console.log("Clicou na li");
+//   });
+// });
 
- const ul = document.querySelector('ul')
+lis.forEach((li) => {
+  li.addEventListener("click", (event) => {
+    const clickedElement = event.target;
 
- const button = document.querySelector('button')
-
- button.addEventListener('click', () => {
-   const li = document.createElement('li')
-
-   li.textContent = 'Novo Item'
-   ul.prepend(li)
- })
-
-//  const lis = document.querySelectorAll('li')
-
-//  lis.forEach(li => {
-//    li.addEventListener('click', event => {
-//      const clickedElement = event.target
-//     console.log('Clicou na LI')
-
-//     event.stopPropagation()
-
-//      clickedElement.remove()
-//    })
-//  })
-
- ul.addEventListener('click', event => {
-   const clickedElement = event.target
-
-   if(clickedElement.tagName === 'LI') {
-     clickedElement.remove()
-   }
- })
+    clickedElement.style.textDecoration = "line-through";
+  });
+});
