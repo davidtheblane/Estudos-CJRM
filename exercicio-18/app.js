e5a38494cdf04320fa34e978e87fe3b391c026eb
davidtheblane <davidtheblane@gmail.com>
2021-08-30 14:54:26 -0300

    RestartingEx19

--- exercicio-18/app.js
+++ exercicio-18/app.js
@@ -20,15 +20,55 @@
   
   Dica: pesquise pelo método "insertAdjacentElement", no MDN;
 */
+const form = document.querySelector("form");
+// const inputUsername = document.querySelector("#username");
+const button = document.querySelector("button");
+const isAValidUsername = (username) => /^[a-zA-z]{6,}$/.test(username);
+let p = document.createElement("p");
 
+//INPUT VALIDATION
+const inputValidation = (event) => {
+  const username = event.target.value;
+  button.insertAdjacentElement("afterend", p);
+
+  if (isAValidUsername(username)) {
+    p.setAttribute("class", "username-success-feedback");
+    p.textContent = "Username válido =)";
+    console.log("Valor Válido");
+    return;
+  }
+  console.log("Valor Inválido");
+  p.setAttribute("class", "username-help-feedback");
+  p.textContent = `O valor deve conter no mínimo 6 caracteres,
+   com apenas letras maiúsculas e/ou minúsculas`;
+};
+
+//SUBMIT VALIDATION
+const submitValidation = (event) => {
+  event.preventDefault();
+  const username = event.target.username.value;
+  const submitFeedback = document.createElement("p");
+  button.insertAdjacentElement("afterend", submitFeedback);
+
+  if (isAValidUsername(username)) {
+    submitFeedback.setAttribute("class", "submit-success-feedback");
+    submitFeedback.textContent = "Dados enviados =)";
+    return;
+  }
+  submitFeedback.setAttribute("class", "submit-help-feedback");
+  submitFeedback.textContent = "Por favor, insira um username válido";
+};
+
+form.addEventListener("keyup", inputValidation);
+form.addEventListener("submit", submitValidation);
 /*
   02
 
-  - Valide o envio do form;
+  - Valide o envio do form;Por favor, insira um username válido
   - Se o username inserido no input é válido, no envio do form, exiba um  
     parágrafo verde abaixo do botão com a mensagem "Dados enviados =)";
   - Se no momento do envio, o valor do input é inválido, o parágrafo deve ser  
-    vermelho e exibir "Por favor, insira um username válido".
+    vermelho e exibir "".
   - Use as classes disponíveis no arquivo style.css para colorir o parágrafo;
   - Não insira o parágrafo manualmente no index.html.
 */
@@ -53,3 +93,15 @@
   Spoiler alert: este tipo de exercício será frequente em etapas mais avançadas  
   do curso, onde falaremos sobre TDD. Vá se aquecendo =D
 */
+
+const some = (numbers = [], item) => {
+  if (numbers.includes(item)) {
+    if (item > 2) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  }
+};
+
+some([1, 2, 3]);
